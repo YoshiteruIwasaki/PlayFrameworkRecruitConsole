7582343b7806d7e2b68adc5d832afbacf42ce5ce
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2014-02-22 02:54:59 +0900

    Issue #9

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,17 @@
 package controllers;
 
+import java.util.List;
+
+import models.beans.SiteBean;
 import play.mvc.Controller;
 import play.mvc.Result;
+import services.beans.SiteBeanService;
 
 public class Application extends Controller {
-    
+
     public static Result index() {
-        return ok(views.html.index.render("Hello Play Framework"));
+    	List<SiteBean> resultList = SiteBeanService.getSiteBeanResultList(0);
+        return ok(views.html.index.render("Hello Play Framework", resultList));
     }
-    
+
 }
