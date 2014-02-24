11c4edb327ab8bff14b230f2ffada65d0ce7671d
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2014-02-24 23:23:42 +0900

    closed #10 タグ調整

--- modules/base/app/utils/base/ApplicationConfigUtils.java
+++ modules/base/app/utils/base/ApplicationConfigUtils.java
@@ -2,7 +2,7 @@ package utils.base;
 
 public class ApplicationConfigUtils {
 
-	public static final int MAX_PER_PAGE = 10;
+	public static final int MAX_PER_PAGE = 12;
 
 	public static final String HEARTRAILS_URL = "http://capture.heartrails.com/400x300?";
 
