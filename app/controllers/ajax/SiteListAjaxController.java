4ed947ec48f43811e69f7052c0ff405fb0ecb531
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2014-02-27 22:51:02 +0900

    closed #12 class名変更

--- app/controllers/ajax/SiteListAjaxController.java
+++ app/controllers/ajax/SiteListAjaxController.java
@@ -6,13 +6,12 @@ import models.beans.SiteBean;
 import play.mvc.Controller;
 import play.mvc.Result;
 import services.beans.SiteBeanService;
-import utils.ApplicationSiteConfigUtils;
 
-public class ListAjaxController extends Controller {
+public class SiteListAjaxController extends Controller {
 
 	public static Result index(int page) {
 		List<SiteBean> resultList = SiteBeanService.getSiteBeanResultList(page);
-		return resultList.size() == 0 ? ok("") : ok(views.html.ajax.listAjax
+		return resultList.size() == 0 ? ok("") : ok(views.html.ajax.siteListAjax
 				.render(page, resultList));
 	}
 
