199f44efb157bbd9b124e1a36bbcec4e1348da36
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2014-03-01 18:10:59 +0900

    Closed #23

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,8 +5,8 @@
 
 create table site (
   site_id                   bigint auto_increment not null,
-  title                     varchar(255),
-  url                       longtext,
+  title                     varchar(255) not null,
+  url                       longtext not null,
   create_date               datetime not null,
   update_date               datetime not null,
   constraint pk_site primary key (site_id))
